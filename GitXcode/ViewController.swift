20edfe9f97ac5cf04264bead503410e46eaaa9c9
sanjay kumawat <sanjaykumawat@sanjays-MacBook-Pro.local>
2016-10-03 15:35:50 +0530

    Initial Commit

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by sanjay kumawat on 11/07/1938 Saka.
+//  Copyright © 1938 Saka KSTECH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
